419832eae3e741ee417d3b2946c0b777f3e69eed
jason <jason.perlmutter@gmail.com>
2026-01-28 20:49:06 -0500

    ok its working locally fwiw

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def serve_sentence(sentence_id):
         sf.write(fp, default_sound, 22050, format='WAV') #MP3') #WAV')
         fp.seek(0)
         print("Caught an exception:", type(e).__name__, e)
-        raise  # optional, re-raise if you want to see the full traceback
+        #raise  # optional, re-raise if you want to see the full traceback
         return send_file(fp, mimetype="audio/wav") #mpeg") #wav")
 
 @app.route('/')
